1d9c259194c533d5855688daade17b0736c84062
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2024-02-06 17:27:20 +0530

    tic tac toe game container created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
-
 function App() {
-    return <h1>Coming Soon...</h1>;
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <li>
+                        <span className="player-name">Player 1</span>
+                        <span className="palyer-symbol">X</span>
+                    </li>
+                    <li>
+                        <span className="player-name">Player 2</span>
+                        <span className="palyer-symbol">O</span>
+                    </li>
+                </ol>
+                GAME BOARD
+            </div>
+            LOG
+        </main>
+    );
 }
 
 export default App;
