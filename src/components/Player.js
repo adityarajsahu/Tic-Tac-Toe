6bc99077b4289feb69b45f35b3eaafdc7a5f2ac4
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2024-02-07 20:48:57 +0530

    edit name and save done

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,25 +1,30 @@
 import { useState } from "react";
 
 const Player = ({ name, symbol }) => {
+    const [playerName, setPlayerName] = useState(name);
     const [isEditing, setIsEditing] = useState(false);
 
     const handleEditClick = () => {
-        setIsEditing(true);
+        setIsEditing((editing) => !editing);
     };
 
-    let playerName = <span className="player-name">{name}</span>;
+    const handleChange = (event) => {
+        setPlayerName(event.target.value);
+    };
+
+    let editablePlayername = <span className="player-name">{playerName}</span>;
 
     if (isEditing) {
-        playerName = <input type="text" required />;
+        editablePlayername = <input type="text" value={playerName} onChange={handleChange} required />;
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
-                <span className="palyer-symbol">{symbol}</span>
+                {editablePlayername}
+                <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 };
