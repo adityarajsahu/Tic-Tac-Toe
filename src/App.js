5bffcd5999244a88641e7fc1e093df819f36cdc4
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2024-02-10 22:16:04 +0530

    Game Over modal done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import GameOver from "./components/GameOver";
 
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 
@@ -49,6 +50,8 @@ function App() {
         }
     }
 
+    const hasDraw = gameTurns.length === 9 && !winner;
+
     const handleSelectSquare = (rowIndex, colIndex) => {
         // setActivePlayer((currentActivePlayer) => (currentActivePlayer === "X" ? "O" : "X"));
         setGameTurns((prevGameTurns) => {
@@ -70,7 +73,7 @@ function App() {
                     <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
                     <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
                 </ol>
-                {winner && <p>You won, {winner}!</p>}
+                {(winner || hasDraw) && <GameOver winner={winner} />}
                 <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
             </div>
             <Log turns={gameTurns} />
